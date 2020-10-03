80221d8dbd447bac5e6f2260279c13606c067655
Miro Kropacek <miro.kropacek@gmail.com>
2020-10-03 20:51:44 +0200

    Correct network mask (but it has no effect on my current problem)

--- enable_tun.sh
+++ enable_tun.sh
@@ -5,7 +5,7 @@ sleep 1
 ip tuntap add dev tap0 mode tap user mikro
 sleep 1
 #ifconfig tap0 192.168.251.1 pointopoint 192.168.251.2 netmask 255.255.255.255 up
-ip address add 192.168.251.1/24 dev tap0
+ip address add 192.168.251.1/32 dev tap0
 ip tunnel add aranym mode ipip local 192.168.251.1 remote 192.168.251.2
 ip link set aranym up
 iptables -t nat -A POSTROUTING -s 192.168.251.2 -j MASQUERADE
