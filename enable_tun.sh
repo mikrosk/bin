062542d7d321c7fa567f28ec7e3027d183eaafe4
Miro Kropacek <miro.kropacek@gmail.com>
2021-01-24 10:16:46 +0100

    Not sure why I had .255 there...

--- enable_tun.sh
+++ enable_tun.sh
@@ -4,8 +4,8 @@ sleep 1
 #tunctl -t tap0 -u mikro
 ip tuntap add dev tap0 mode tap user mikro
 sleep 1
-#ifconfig tap0 192.168.251.1 pointopoint 192.168.251.2 netmask 255.255.255.255 up
-ip address add 192.168.251.1/32 dev tap0
+#ifconfig tap0 192.168.251.1 pointopoint 192.168.251.2 netmask 255.255.255.0 up
+ip address add 192.168.251.1/24 dev tap0
 ip tunnel add aranym mode ipip local 192.168.251.1 remote 192.168.251.2
 ip link set aranym up
 iptables -t nat -A POSTROUTING -s 192.168.251.2 -j MASQUERADE
